ce18169aef7fb631c1f6f19813b4e669afaf0db8
v-teja <tejaviswa95@gmail.com>
2023-05-18 18:47:46 +0530

    another attempt

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-      <p>I am learning React. My life is getting better.</p>
+    <div id="main">
+        <p>I am learning React. My life is getting better.</p>
+    </div>
+      
   )
 }
 
